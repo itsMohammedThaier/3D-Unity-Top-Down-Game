32a0d92cb981a910f58fa6316395cc310cf092a7
itsMohammedThaier <mohamadthaier2@gmail.com>
2021-01-14 22:58:39 +0300

    Adding material

---
+++ Assets/Furnitures/Scripts/RotationChanger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationChanger : MonoBehaviour
+{
+    public Transform whoWillRotate;
+    public bool ActiveTheRotationMovement;
+    public float xRotatingSpeed;
+    public float yRotatingSpeed;
+    public float zRotatingSpeed;
+    public Vector3 test;
+    private Vector3 nextRotate;
+    void Start()
+    {
+        whoWillRotate = (whoWillRotate != null)? whoWillRotate: transform;
+        nextRotate = whoWillRotate.rotation.eulerAngles;
+    }
+
+    void Update()
+    {
+        if (ActiveTheRotationMovement)
+        {
+            RotateTheObject();
+        }
+    }
+    public void RotateTheObject()
+    {
+        nextRotate += new Vector3(xRotatingSpeed, yRotatingSpeed, zRotatingSpeed);
+        Quaternion nextFrameRotate = Quaternion.Euler(nextRotate);
+        whoWillRotate.rotation = nextFrameRotate;
+        if (whoWillRotate.rotation == Quaternion.Euler(Vector3.zero))
+        {
+            ResetRotation();
+        }
+
+    }
+    private void ResetRotation()
+    {
+        whoWillRotate.rotation = Quaternion.Euler(Vector3.zero);
+    }
+}
