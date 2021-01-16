54e2003d767ba28905d55e3ade36b7e50c2c610a
itsMohammedThaier <mohamadthaier2@gmail.com>
2021-01-16 06:12:32 +0300

    t

---
+++ Assets/Grounds/Scripts/SocialMediaButtons.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SocialMediaButtons : MonoBehaviour
+{
+    public string websiteLink;
+    private void OnCollisionEnter(Collision other) 
+    {
+        if (other.collider.tag == "Player")
+            EnterWebsite();
+    }
+
+    public void EnterWebsite()
+    {
+        Application.OpenURL(websiteLink);
+    }
+}
